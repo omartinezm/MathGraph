b326b89388c7f8e2dacee94a6380598c6847557d
Otaivin Martínez <otaivin@gmail.com>
2020-11-01 03:59:34 -0500

    Update draw_graph.py
    Generalized to any graph

--- draw_graph.py
+++ draw_graph.py
@@ -4,19 +4,26 @@ from pyglet.gl import *
 from pyglet import shapes
 from win32api import GetSystemMetrics
 from tqdm import tqdm,tqdm_gui
+from math import cos, sin, pi
 
 ZOOM_IN_FACTOR = 1.2
 ZOOM_OUT_FACTOR = 1/ZOOM_IN_FACTOR
 
 class DrawGraph(pyglet.window.Window):
     
-    def __init__(self, neuralNet, width=0, height=0, x_sep=80, y_sep= 80, *args, **kwargs):
-        self.nn=neuralNet
+    def __init__(self, graph, width=0, height=0, x_sep=80, y_sep= 80, *args, **kwargs):
+        self.graph=graph
         self.separation_x=x_sep
         self.separation_y=y_sep
         conf=Config(samples=4,depth_size=16)
-        super().__init__(min([self.separation_x*(self.nn.nlayers),GetSystemMetrics(0)]),
-                         min([self.separation_y*(max(self.nn.npl)+1),GetSystemMetrics(1)]),
+        if(self.graph.kind=="NeuralNetFC"):
+            w=self.separation_x*(self.graph.nlayers)
+            h=self.separation_y*(max(self.graph.npl)+1)
+        else:
+            w=700
+            h=700
+        super().__init__(min([w,1000]),
+                         min([h,700]),
                          config=conf,caption='Graph',resizable=False,
                          style=pyglet.window.Window.WINDOW_STYLE_DIALOG,
                          *args, **kwargs)
@@ -75,34 +82,56 @@ class DrawGraph(pyglet.window.Window):
             self.top=mouse_y_in_world+(1-mouse_y)*self.zoomed_height
 
     def __create_batch__(self):
-        acum_l=0
-        acum_x=0
-        x_pos=self.separation_x/2
-        y_pos=self.separation_y
-        total_n=sum(self.nn.npl)
-        c=(0, 0, int(255*(1-acum_l/total_n)),255)
-        #for l in tqdm(self.nn.npl,desc="Drawing graph"):
-        for l in self.nn.npl:
-            if acum_l!=0: c=(int(255*((acum_x)/self.nn.nlayers)), 0, int(255*(1-(acum_x)/self.nn.nlayers)), 255)
-            for n in range(l):
-                self.vertex.append(pyglet.text.Label(self.nn.get_vertexes()[acum_l+n],
+        if self.graph.kind=="NeuralNetFC":
+            acum_l=0
+            acum_x=0
+            x_pos=self.separation_x/2
+            y_pos=self.separation_y
+            total_n=sum(self.graph.npl)
+            c=(0, 0, int(255*(1-acum_l/total_n)),255)
+            for l in self.graph.npl:
+                if acum_l!=0: c=(int(255*((acum_x)/self.graph.nlayers)), 0, int(255*(1-(acum_x)/self.graph.nlayers)), 255)
+                for n in range(l):
+                    self.vertex.append(pyglet.text.Label(self.graph.get_vertexes()[acum_l+n],
                                                 font_name='Arial',font_size=12,bold=True,
                                                 x=x_pos+(acum_x)*self.separation_x, y=y_pos+(n)*self.separation_y,
                                                 anchor_x='center', anchor_y='center',color=c,
                                                 batch=self.batch,group=self.foreground))
-                self.circles.append(pyglet.shapes.Circle(x=self.vertex[-1].x,
+                    self.circles.append(pyglet.shapes.Circle(x=self.vertex[-1].x,
                                                          y=self.vertex[-1].y,
                                                          radius=25,color=(255, 255, 255),
                                                          batch=self.batch,group=self.background))
-                if acum_x!=0:
-                    for j in range(self.nn.npl[acum_x-1]):
-                        self.lines.append(shapes.Line(self.vertex[acum_l-self.nn.npl[acum_x-1]+j].x,
-                                                      self.vertex[acum_l-self.nn.npl[acum_x-1]+j].y,
+                    if acum_x!=0:
+                        for j in range(self.graph.npl[acum_x-1]):
+                            self.lines.append(shapes.Line(self.vertex[acum_l-self.graph.npl[acum_x-1]+j].x,
+                                                      self.vertex[acum_l-self.graph.npl[acum_x-1]+j].y,
                                                       self.vertex[-1].x,self.vertex[-1].y,
                                              width=5, color=c[:-1], batch=self.batch,group=self.background_lines))
-            acum_l+=l
-            acum_x+=1
-            
+                acum_l+=l
+                acum_x+=1
+        else:
+            angle=0
+            theta=2*pi/(len(self.graph.vertex))
+            self.circles={}
+            for ver in self.graph.vertex:
+                pyglet.text.Label(ver.get_name(),
+                                  font_name='Arial',font_size=12,bold=False,
+                                  x=self.width/2+(self.width/2-100)*cos(angle),
+                                  y=self.height/2+(self.height/2-100)*sin(angle),
+                                  anchor_x='center', anchor_y='center',color=(0, 0, 255,255),
+                                  batch=self.batch,group=self.foreground)          
+                self.circles[ver.get_name()]=pyglet.shapes.Circle(x=self.width/2+(self.width/2-100)*cos(angle),
+                                                         y=self.height/2+(self.height/2-100)*sin(angle),
+                                                         radius=25,color=(255, 255, 255),
+                                                         batch=self.batch,group=self.background)
+                angle+=theta
+            for edge in self.graph.edges:
+                self.lines.append(shapes.Line(self.circles[edge.start.get_name()].x,
+                                              self.circles[edge.start.get_name()].y,
+                                              self.circles[edge.end.get_name()].x,
+                                              self.circles[edge.end.get_name()].y, width=5, color=(255, 55, 55),
+                                              batch=self.batch,group=self.background_lines))
+                
     def on_draw(self):
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
